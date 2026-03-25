a3f12bbe71d6bcb11fe5b34680cb324cdf1c8fe7
Atharva Ghongade <atharvaghongade22@gmail.com>
2026-03-25 12:34:56 +0530

    removed src/screens - shit gave me a scare man

--- frontend/src/screens/index.ts
+++
@@ -1,9 +0,0 @@
-// Screens index
-export { default as OnboardingScreen } from './OnboardingScreen';
-export { default as HomeScreen } from './HomeScreen';
-export { default as MoodScreen } from './MoodScreen';
-export { default as JournalScreen } from './JournalScreen';
-export { default as ChatScreen } from './ChatScreen';
-export { default as InsightsScreen } from './InsightsScreen';
-export { default as GuideScreen } from './GuideScreen';
-export { default as ProfileScreen } from './ProfileScreen';
